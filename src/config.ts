cedc7e30094015bb1fc1f85c032d3be011c84bd7
xiaolidan00 <764937567@qq.com>
2026-01-09 17:31:33 +0800

    fix: 重新播放当前文本

--- src/config.ts
+++ src/config.ts
@@ -153,6 +153,8 @@ export const refreshSpeak = () => {
   if (beforeRange) {
     removeHighlight(beforeRange);
   }
+  isPlay.value = true;
+  startSpeak();
 };
 export const stopSpeak = () => {
   isPlay.value = false;
